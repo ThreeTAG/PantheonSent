10823609ed466e09a9180b2d889d16dcb82f0098
Lucraft <lucasbandermann@gmail.com>
2022-10-02 22:18:21 +0200

    Fixed archeologist zombie texture, fixed trades, hid MC cape when being MK

--- common/src/main/java/net/threetag/pantheonsent/entity/PSVillagerProfessions.java
+++ common/src/main/java/net/threetag/pantheonsent/entity/PSVillagerProfessions.java
@@ -5,8 +5,12 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.threetag.palladium.entity.BasicItemListing;
 import net.threetag.palladiumcore.registry.DeferredRegister;
 import net.threetag.palladiumcore.registry.RegistrySupplier;
+import net.threetag.palladiumcore.registry.VillagerTradeRegistry;
 import net.threetag.pantheonsent.PantheonSent;
 
 import java.util.function.Predicate;
@@ -21,10 +25,9 @@ public class PSVillagerProfessions {
             () -> new VillagerProfession("archeologist", POI_PREDICATE, POI_PREDICATE, ImmutableSet.of(), ImmutableSet.of(), null));
 
     public static void init() {
-        // TODO TradeRegistry in core
-//        TradeRegistry.registerVillagerTrade(ARCHEOLOGIST.get(), 1,
-//                new BasicItemListing(1, new ItemStack(Items.BONE, 6), 3, 2),
-//                new BasicItemListing(1, new ItemStack(Items.BONE_BLOCK, 3), 2, 2));
+        VillagerTradeRegistry.registerForProfession(ARCHEOLOGIST.get(), 1,
+                new BasicItemListing(1, new ItemStack(Items.BONE, 6), 3, 2),
+                new BasicItemListing(1, new ItemStack(Items.BONE_BLOCK, 3), 2, 2));
     }
 
 }
