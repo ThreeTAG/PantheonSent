dac69e53b5b780314006c5da94d98888083c5b47
Lucraft <lucasbandermann@gmail.com>
2023-07-21 23:43:15 +0200

    Improved dart throw ability

--- common/src/main/java/net/threetag/pantheonsent/PantheonSentDebug.java
+++ common/src/main/java/net/threetag/pantheonsent/PantheonSentDebug.java
@@ -3,12 +3,13 @@ package net.threetag.pantheonsent;
 public class PantheonSentDebug {
 
     public static void init() {
-//        InteractionEvent.RIGHT_CLICK_ITEM.register((player, hand) -> {
-//            if (!player.level.isClientSide && player.getItemInHand(hand).getItem() == Items.BONE) {
-//                Khonshu khonshu = new Khonshu(player.level, player, true);
-//                player.level.addFreshEntity(khonshu);
+//        LivingEntityEvents.TICK.register(entity -> {
+//            if (entity instanceof Player player) {
+//                AbilityReference reference = new AbilityReference(PantheonSent.id("moon_knight"), "throw_dart");
+//                var dependency = reference.getEntry(player);
+//                var res = dependency != null && (dependency.cooldown > 0);
+//                player.displayClientMessage(Component.literal(String.valueOf(res)), true);
 //            }
-//            return CompoundEventResult.pass();
 //        });
     }
 
