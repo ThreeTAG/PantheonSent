7f94c137b7adf14bd6e72269093a8bf200e6bf4d
Lucas Bandermann <lucasbandermann@gmail.com>
2024-02-11 17:21:52 +0100

    Removed some leftover code for removed enchantment

--- common/src/main/java/net/threetag/pantheonsent/mixin/EnchantmentHelperMixin.java
+++ common/src/main/java/net/threetag/pantheonsent/mixin/EnchantmentHelperMixin.java
@@ -1,10 +1,7 @@
 package net.threetag.pantheonsent.mixin;
 
 import com.google.common.collect.Lists;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import net.threetag.pantheonsent.item.PSItems;
@@ -24,20 +21,6 @@ public abstract class EnchantmentHelperMixin {
         if (stack.getItem() == PSItems.KHONSHU_USHABTI.get()) {
             List<EnchantmentInstance> list = Lists.newArrayList();
             list.add(new EnchantmentInstance(PSEnchantments.GODLY_ENCAPSULATING.get(), 1));
-            ci.setReturnValue(list);
-        } else if (stack.getItem() == Items.BRUSH) {
-            List<EnchantmentInstance> list = Lists.newArrayList();
-            for (Enchantment enchantment : BuiltInRegistries.ENCHANTMENT) {
-                if ((!enchantment.isTreasureOnly() || allowTreasure) && enchantment.isDiscoverable() && enchantment.canEnchant(stack)) {
-                    for (int i = enchantment.getMaxLevel(); i > enchantment.getMinLevel() - 1; --i) {
-                        if (level >= enchantment.getMinCost(i) && level <= enchantment.getMaxCost(i)) {
-                            list.add(new EnchantmentInstance(enchantment, i));
-                            break;
-                        }
-                    }
-                }
-            }
-
             ci.setReturnValue(list);
         }
     }
